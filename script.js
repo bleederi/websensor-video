bcf9ba5b90321bdb861689fb01ae39059813a1be
Jesse Nieminen <jesse.nieminen@intel.com>
2017-06-07 09:30:44 +0300

    Worked on data capture and processing
    Worked on data capture and processing
    
    Worked on data capture and processing
    
    Worked on data capture and processing
    
    Worked on data capture and processing
    
    Worked on data capture and processing
    
    Worked on data capture and processing
    
    Worked on data capture and processing

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
 //Javascript function to convert from sensor readings (one for each reading), to sequences (one for each coordinate)
-function toCoordSeq(buffer, key)
+function toCoordSeq(buffer)
 {
-        key = 'acceleration';
-        seq = buffer[key];        //Acceleration data buffer, sequence of xyz readings
-/*
-                #Make sequences for each key
-                #First handle list keys
-                if key in listkeys and key in xyzkeys and key not in numpyarrkeys:
-                        seq_x = []
-                        seq_y = []
-                        seq_z = []
-                        for i in value:
-                                seq_x.append(i['x'])
-                                seq_y.append(i['y'])
-                                seq_z.append(i['z'])
-                        seq = {'x':seq_x, 'y':seq_y, 'z':seq_z}
-                elif key in listkeys and key in abgkeys and key not in numpyarrkeys:
-                        seq_alpha = []
-                        seq_beta = []
-                        seq_gamma = []
-                        for i in value:
-                                seq_alpha.append(i['alpha'])
-                                seq_beta.append(i['beta'])
-                                seq_gamma.append(i['gamma'])
-                        seq = {'alpha':seq_alpha, 'beta':seq_beta, 'gamma':seq_gamma}
-                elif key in listkeys and key in numpyarrkeys:
-                        seqlist = value
-                #Then handle dict keys
-                elif key in dictkeys and key in xyzkeys:
-                        seq = {'x':value['x'], 'y':value['y'], 'z':value['z']}
-                elif key in dictkeys and key in abgkeys:
-                        seq = {'alpha':value['alpha'], 'beta':value['beta'], 'gamma':value['gamma']}
-                else:   #DAC keys
-                        data[key] = value
-                if(seq):
-                    data[key] = seq
-                elif(seqlist):
-                    data[key] = seqlist
-        buttondata_array.append(data)
-*/
+        seq_x = [];
+        seq_y = [];
+        seq_z = [];
+        for (var i in buffer)
+        {
+                seq_x.push(buffer[i]['x']);
+                seq_y.push(buffer[i]['y']);
+                seq_z.push(buffer[i]['z']);
+        }
+        seq = {'x':seq_x, 'y':seq_y, 'z':seq_z};
         return seq;
 }
