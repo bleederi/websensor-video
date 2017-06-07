9fda58674ed21660e9e23e79856b1c04e413a7a2
Jesse Nieminen <jesse.nieminen@intel.com>
2017-06-07 10:46:48 +0300

    Worked on step detection

--- script.js
+++ script.js
@@ -53,9 +53,8 @@ function slice(obj, start, end) {
 function stepDetection(seq)      //Returns 1 if there was a step in the given sequence, otherwise 0
 {
         //console.log(seq);
-        let maxval = {'x':Math.max(seq['x']), 'y':Math.max(seq['y']), 'z':Math.max(seq['z'])};
-        let minval = {'x':Math.min(seq['x']), 'y':Math.min(seq['y']), 'z':Math.min(seq['z'])};
-        console.log(seq['x']);
+        let maxval = {'x':Math.max.apply(null, (seq['x'])), 'y':Math.max.apply(null, (seq['y'])), 'z':Math.max.apply(null, (seq['z'])),};
+        let maxval = {'x':Math.min.apply(null, (seq['x'])), 'y':Math.min.apply(null, (seq['y'])), 'z':Math.min.apply(null, (seq['z'])),};
         console.log(maxval['x'] - minval['x'], maxval['y'] - minval['y'], maxval['z'] - minval['z']);
         return 0;
 }
