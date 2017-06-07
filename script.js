8170a82ee652613d0e7fd52342b5d62c372db34f
Jesse Nieminen <jesse.nieminen@intel.com>
2017-06-07 10:23:09 +0300

    Worked on data capture and processing

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*
+ * Websensor video project
+ * https://github.com/jessenie-intel/web-sensor-js-privacy
+ *
+ * Copyright (c) 2017 Jesse Nieminen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 //Javascript function to convert from sensor readings (one for each reading), to sequences (one for each coordinate)
 function toCoordSeq(buffer)
 {
@@ -12,4 +32,30 @@ function toCoordSeq(buffer)
         }
         seq = {'x':seq_x, 'y':seq_y, 'z':seq_z};
         return seq;
+}
+
+/**
+ * Slices the object. Note that returns a new spliced object,
+ * e.g. do not modifies original object. Also note that that sliced elements
+ * are sorted alphabetically by object property name.
+ * Credit to https://stackoverflow.com/a/20682709
+ */
+function slice(obj, start, end) {
+
+    var sliced = {};
+    var i = 0;
+    for (var k in obj) {
+        if (i >= start && i < end)
+            sliced[k] = obj[k];
+
+        i++;
+    }
+
+    return sliced;
+}
+
+function stepDetection(seq)      //Returns 1 if there was a step in the given sequence, otherwise 0
+{
+        console.log(seq);
+        return 0;
 }
