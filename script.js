21b17264437072fc3dc82648872c0c1b8fc34404
Jesse Nieminen <jesse.nieminen@intel.com>
2017-06-08 14:29:03 +0300

    Worked on step detection

--- script.js
+++ script.js
@@ -202,7 +202,7 @@ function detectValleys(seq, mode = 'magnitude')
                         let curr = seq[index];
                         let next = seq[index+1];
                         console.log(curr, stepaverage);
-                        if(curr < prev && curr < next && (curr < stepaverage || lastvalleymag == 'null' || lastpeakmag == 'null'))
+                        if(curr < prev && curr < next)  //TODO: lower than stepaverage
                                 {
                                         valleys.push(index);
                                         lastvalleymag = curr;
