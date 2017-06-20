3b986b231262c8d1ed2a961063396341e79ba54c
Jesse Nieminen <jesse.nieminen@intel.com>
2017-06-20 13:35:07 +0300

    Work on walking detection for Pixel

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ function detectPeaksValleys(seq, mode = 'magnitude')
                         let curr = seq[index];
                         let next = seq[index+1];
                         let avg = seq_slice.reduce(function(sum, a) { return sum + a },0)/(seq.length||1);
-                        let variance = Math.pow(standardDeviation(seq_slice))/alpha;
+                        let variance = standardDeviation(seq_slice)/alpha;
                         console.log(variance);
 
                         if(curr > prev && curr > next && (curr > stepaverage || !stepaverage) && curr > (avg+variance))  //peak
