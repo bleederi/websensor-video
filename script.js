58015baa79f4a3e376b8e5f510ac871f7b33829f
Jesse Nieminen <jesse.nieminen@intel.com>
2017-06-20 10:11:09 +0300

    Work on walking detection for Pixel

--- script.js
+++ script.js
@@ -375,7 +375,8 @@ function stepDetection(seq)      //Returns 1 if there was a step in the given se
         stddevpct = stddev / min;
         console.log("Std dev pct", stddevpct);
         console.log("Std dev accel", stddev_accel/min_accel);
-        if(Math.abs(stepamt-stepdiff.length) <= (stepamt) && stepdiff.length > 2)        //stepamt-1 for windows tablet, <=3 for Pixel
+        //if(Math.abs(stepamt-stepdiff.length) <= (stepamt) && stepdiff.length > 2)        //stepamt-1 for windows tablet, <=3 for Pixel
+        if(true)
         {
                 //console.log(max, min);  
                 //if(((max-min)/max) < 0.5)
