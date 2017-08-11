f3e178c0059a4f27d51ac1f3969aada1aa489a0e
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-11 13:09:01 +0300

    Works in landscape

--- script.js
+++ script.js
@@ -316,7 +316,7 @@ function onWindowResize() {
                 }
                 if(screen.orientation.angle === 90 || screen.orientation.angle === 180 || screen.orientation.angle === 270)
                 {
-                        longitude = -orientation_sensor.z - orientation_sensor.longitudeInitial - Math.PI/2;
+                        longitude = -orientation_sensor.z - orientation_sensor.longitudeInitial + 3*Math.PI/2;
                 }
                 if(longitude < 0)       /*When rewinding video, the heading is inverted - this is easier than rendering the video differently on the sphere*/
                 {
