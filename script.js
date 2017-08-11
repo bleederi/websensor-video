89ab39b8e26737b557eeaa9ed328401560163365
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-11 15:08:43 +0300

    Works in landscape

--- script.js
+++ script.js
@@ -324,7 +324,7 @@ function onWindowResize() {
                 }
                 else if(screen.orientation.angle === 90)
                 {
-                        longitude = -orientation_sensor.z - orientation_sensor.longitudeInitial - Math.PI/2;
+                        longitude = -orientation_sensor.z - orientation_sensor.longitudeInitial - Math.PI;
                 }
                 else if(screen.orientation.angle === 270)
                 {
