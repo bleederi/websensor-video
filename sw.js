a974d19fc7e05073ba21c7b6fc967ea8128181c8
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-17 12:05:10 +0300

    Suppress error

--- sw.js
+++ sw.js
@@ -18,24 +18,23 @@ self.addEventListener('install', function(event) {
 
 this.addEventListener('fetch', function(event) {
 //Retrieval from cache
-  event.respondWith(caches.match(event.request).then(function(response) {
-    // caches.match() always resolves
-    // but in case of success response will have value
-    if (response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
-        
-        caches.open(CACHE_VERSION.toString()).then(function (cache) {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      })
-    }
-  }));
+        try {
+                event.respondWith(caches.match(event.request).then(function(response) {
+                if (response !== undefined) {
+                        return response;
+                } else {
+                        return fetch(event.request).then(function (response) {
+                //Response may be used only once so need to save clone to put one copy in cache and serve second one
+                let responseClone = response.clone();
+                caches.open(CACHE_VERSION.toString()).then(function (cache) {
+                        cache.put(event.request, responseClone);
+                });
+                return response;
+                })
+                }
+                }));
+        } catch(err) {
+                console.log("Error:", err);
+        }
 });
 
