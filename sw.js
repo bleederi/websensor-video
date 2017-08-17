d67a5c09fc8f7920b941e8f66ca69c6289e346ec
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-17 17:18:10 +0300

    Caching

--- sw.js
+++ sw.js
@@ -28,22 +28,18 @@ self.addEventListener('fetch', function(event) {
 //Retrieval from cache
     event.respondWith(caches.open(CACHE_NAME + CACHE_VERSION.toString()).then(function(cache) {
         return cache.match(event.request).then(function(response) {
-            //console.log("cache request: " + event.request.url);
             var fetchPromise = fetch(event.request).then(function(networkResponse) {
-                // if we got a response from the cache, update the cache
-                //console.log("fetch completed: " + event.request.url, networkResponse);
+                //If there's a response from the network, update the cache
                 if (networkResponse) {
-                    //console.debug("updated cached page: " + event.request.url, networkResponse);
                     cache.put(event.request, networkResponse.clone());
                 }
                 return networkResponse;
             }, function (e) {
-                // rejected promise - just ignore it, we're offline
-                //console.log("Error in fetch()", e);
+                //Rejected promise will be ignored, it means we're offline
                 ;
             });
 
-            // respond from the cache, or the network
+            //Respond primarily from the cache, or secondarily the network
             return response || fetchPromise;
         });
     }));
