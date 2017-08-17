c99f2d914ca82c19035fd9bcec32b4761f62eb99
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-17 15:19:04 +0300

    revert

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const CACHE_VERSION = 1;
 self.addEventListener('install', function(event) {
 //Caching
   event.waitUntil(
-    caches.open(CACHE_VERSION.toString()).then(function(cache) {
+    caches.open(CACHE_NAME + CACHE_VERSION.toString()).then(function(cache) {
       return cache.addAll([
         'index.html',
         'sw.js',
@@ -26,36 +26,6 @@ self.addEventListener("activate", function(event) {
 
 this.addEventListener('fetch', function(event) {
 //Retrieval from cache
- /* if (event.request.headers.get('range')) {     //Video request
-    var pos =
-    Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
-    console.log('Range request for', event.request.url,
-      ', starting position:', pos);
-    event.respondWith(
-      caches.open(CACHE_NAME + CACHE_VERSION.toString())
-      .then(function(cache) {
-        return cache.match(event.request.url);
-      }).then(function(res) {
-        if (!res) {
-          return fetch(event.request)
-          .then(res => {
-            return res.arrayBuffer();
-          });
-        }
-        return res.arrayBuffer();
-      }).then(function(ab) {
-        return new Response(
-          ab.slice(pos),
-          {
-            status: 206,
-            statusText: 'Partial Content',
-            headers: [
-              ['Content-Type', 'video/webm'],
-              ['Content-Range', 'bytes ' + pos + '-' +
-                (ab.byteLength - 1) + '/' + ab.byteLength]]
-          });
-      }));
-  } else {*/
         event.respondWith(caches.match(event.request).then(function(response) {
                 if (response !== undefined) {
                         return response;
@@ -63,7 +33,7 @@ this.addEventListener('fetch', function(event) {
                         return fetch(event.request).then(function (response) {
                 //Response may be used only once so need to save clone to put one copy in cache and serve second one
                 let responseClone = response.clone();
-                caches.open(CACHE_VERSION.toString()).then(function (cache) {
+                caches.open(CACHE_NAME + CACHE_VERSION.toString()).then(function (cache) {
                                 cache.put(event.request, responseClone).catch(
                                 TypeError, function(e) {}       //Suppress TypeError
                                 );
@@ -72,6 +42,6 @@ this.addEventListener('fetch', function(event) {
                 })
                 }
         }));
-      //  }
 });
+
 
