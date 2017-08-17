6b73c38e107daf68192b5748fe4d57575542cdc2
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-17 17:08:11 +0300

    Caching

--- sw.js
+++ sw.js
@@ -26,22 +26,29 @@ self.addEventListener("activate", function(event) {
 
 this.addEventListener('fetch', function(event) {
 //Retrieval from cache
-        event.respondWith(caches.match(event.request).then(function(response) {
-                if (response !== undefined) {
-                        return response;
-                } else {
-                        return fetch(event.request).then(function (response) {
-                //Response may be used only once so need to save clone to put one copy in cache and serve second one
-                let responseClone = response.clone();
-                caches.open(CACHE_NAME + CACHE_VERSION.toString()).then(function (cache) {
-                                cache.put(event.request, responseClone).catch(
-                                TypeError, function(e) {}       //Suppress TypeError
-                                );
-                });
-                return response;
-                })
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.open(CACHE_NAME + CACHE_VERSION.toString()).then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+            //console.log("cache request: " + event.request.url);
+            var fetchPromise = fetch(event.request).then(function(networkResponse) {
+                // if we got a response from the cache, update the cache
+                //console.log("fetch completed: " + event.request.url, networkResponse);
+                if (networkResponse) {
+                    //console.debug("updated cached page: " + event.request.url, networkResponse);
+                    cache.put(event.request, networkResponse.clone());
                 }
-        }));
+                return networkResponse;
+            }, function (e) {
+                // rejected promise - just ignore it, we're offline
+                //console.log("Error in fetch()", e);
+                ;
+            });
+
+            // respond from the cache, or the network
+            return response || fetchPromise;
+        });
+    }));
 });
+
 
 
