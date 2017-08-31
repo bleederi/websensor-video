67f0d02944d982fe93601a399ec08da798ea7b53
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-31 16:34:57 +0300

    Code cleanup wd

--- scripts/wdalgorithm.js
+++ scripts/wdalgorithm.js
@@ -1,18 +1,5 @@
 // This algorithm is adapted to and tested on a Pixel smartphone
 
-class LowPassFilterData {       //https://w3c.github.io/motion-sensors/#pass-filters
-  constructor(reading, bias) {
-    Object.assign(this, { x: reading.x, y: reading.y, z: reading.z });
-    this.bias = bias;
-  }
-
-    update(reading) {
-        this.x = this.x * this.bias + reading.x * (1 - this.bias);
-        this.y = this.y * this.bias + reading.y * (1 - this.bias);
-        this.z = this.z * this.bias + reading.z * (1 - this.bias);
-    }
-}
-
 // The walking detection algorithm
 // Device dependent, tested on a Pixel smartphone
 var ALGORITHM = (function () {
